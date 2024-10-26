4fb4a04d0dd0f4e99a5d620ba9ffce4fbf864cd9
T_cat9000_2 <terpstragraham@gmail.com>
2024-10-26 14:10:03 -0700

    add new utility

--- src/wrapper.py
+++ src/wrapper.py
@@ -8,7 +8,7 @@ username = input("Enter Scratch username: ")
 
 while True:
   # get api wrapping utility
-  utility = input("Utility [user-projects-wrapper, follower-wrapping-utility, exit]: ")
+  utility = input("Utility? [user-projects-wrapper, follower-wrapper, favorite-projects-wrapper, exit]: ")
   
   if utility == "user-projects-wrapper":
     # url
@@ -21,7 +21,7 @@ while True:
     # display info
     for project in data:
         title = project.get('title', 'N/A')
-        description = project.get('description', 'No description available')
+        description = project.get('description')
         loves = project.get('stats', {}).get('loves', 0)
         favorites = project.get('stats', {}).get('favorites', 0)
         views = project.get('stats', {}).get('views', 0)
@@ -45,7 +45,7 @@ while True:
         print("=" * 50)
         print("\n")
   
-  elif utility == "follower-wrapping-utility":
+  elif utility == "follower-wrapper":
     # url
     url = f"https://api.scratch.mit.edu/users/{username}/followers/"
     # get data
@@ -66,6 +66,39 @@ while True:
         # If you want to fetch and display more follower details, add additional fields here
         print("=" * 50)
         print("\n")
+      
+  elif utility == "favorite-projects-wrapper":
+    # url
+    url = f"https://api.scratch.mit.edu/users/{username}/favorites/"
+    # get data
+    response = urlib.request.urlopen(url)
+    data = json.loads(response.read())
+    # display info
+    print("=" * 50)
+    print(f"Favorite projects for {username}")
+    print("=" * 50)
+
+    for project in data:
+     title = project.get('title', 'N/A')
+     description = project.get('description')
+      
+     print("=" * 50)
+     print("PROJECT NAME")
+     print("-" * 50)
+     print(f"{title}")
+        
+     print("\nDESCRIPTION")
+     print("-" * 50)
+     print(f"{description}")
+        
+     print("\nSTATS")
+     print("-" * 50)
+     print(f"Hearts: {loves}")
+     print(f"Stars: {favorites}")
+     print(f"Views: {views}")
+     print(f"Remixes: {remixes}")
+     print("=" * 50)
+     print("\n")
     
   elif utility == "exit":
     os.exit()
